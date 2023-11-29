06e9c01e7566e214198eb7103f0000296d7d4a58
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-11-29 01:05:41 +0000

    feat: efficient time using dayjs & added prettier
    > npx prettier . --write
    .eslintrc.json 87ms
    .vscode/extensions.json 6ms
    .vscode/launch.json 4ms
    CHANGELOG.md 59ms
    jsconfig.json 7ms
    package-lock.json 60ms (unchanged)
    package.json 6ms (unchanged)
    README.md 40ms
    src/extension.js 34ms
    src/getTimeLeft.js 91ms
    src/statusBar.js 10ms
    test/runTest.js 5ms
    test/suite/extension.test.js 5ms
    test/suite/index.js 18ms
    vsc-extension-quickstart.md 209ms

--- src/extension.js
+++ src/extension.js
@@ -5,37 +5,37 @@ let disposable;
 let subscriptions = [];
 
 function activate() {
-    disposable = vscode.commands.registerCommand("xmasTime.run", function () {
-        updateStatusBar();
-        vscode.window.showInformationMessage(
-            "The status bar was updated successfully."
-        );
-    });
-
-    // Register a command and a status bar item
-    subscriptions.push(disposable);
-    subscriptions.push(createStatusBar());
-
-    // Register some listeners with disposables
-    subscriptions.push(
-        vscode.window.onDidChangeTextEditorSelection(updateStatusBar)
-    );
-    subscriptions.push(
-        vscode.window.onDidChangeActiveTextEditor(updateStatusBar)
-    );
-    subscriptions.push(vscode.workspace.onDidSaveTextDocument(updateStatusBar));
-
-    // Update the status bar once at start
+  disposable = vscode.commands.registerCommand("xmasTime.run", function () {
     updateStatusBar();
+    vscode.window.showInformationMessage(
+      "The status bar was updated successfully.",
+    );
+  });
+
+  // Register a command and a status bar item
+  subscriptions.push(disposable);
+  subscriptions.push(createStatusBar());
+
+  // Register some listeners with disposables
+  subscriptions.push(
+    vscode.window.onDidChangeTextEditorSelection(updateStatusBar),
+  );
+  subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor(updateStatusBar),
+  );
+  subscriptions.push(vscode.workspace.onDidSaveTextDocument(updateStatusBar));
+
+  // Update the status bar once at start
+  updateStatusBar();
 }
 
 function deactivate() {
-    // Disposing
-    disposable.dispose();
-    subscriptions.forEach((subscription) => subscription.dispose());
+  // Disposing
+  disposable.dispose();
+  subscriptions.forEach((subscription) => subscription.dispose());
 }
 
 module.exports = {
-    activate,
-    deactivate,
+  activate,
+  deactivate,
 };
--- src/statusBar.js
+++ src/statusBar.js
@@ -7,30 +7,30 @@ const { getTimeLeft } = require("./getTimeLeft");
 let statusBar = null;
 
 function createStatusBar() {
-    statusBar = vscode.window.createStatusBarItem(
-        vscode.StatusBarAlignment.Right
-    );
-    statusBar.command =
-        getTimeLeft() !== "Merry Christmas!" &&
-        getTimeLeft() !== "Christmas are over!"
-            ? "xmasTime.run"
-            : null;
+  statusBar = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+  );
+  statusBar.command =
+    getTimeLeft() !== "Merry Christmas!" &&
+    getTimeLeft() !== "Christmas are over!"
+      ? "xmasTime.run"
+      : null;
 
-    return statusBar;
+  return statusBar;
 }
 
 function updateStatusBar() {
-    statusBar.text =
-        getTimeLeft() !== "Merry Christmas!" &&
-        getTimeLeft() !== "Christmas are over!"
-            ? `$(watch) ${getTimeLeft()}`
-            : `$(sparkle) ${getTimeLeft()}`;
-    statusBar.tooltip =
-        getTimeLeft() !== "Merry Christmas!" &&
-        getTimeLeft() !== "Christmas are over!"
-            ? `${getTimeLeft()} left until Christmas 🎄`
-            : null;
-    statusBar.show();
+  statusBar.text =
+    getTimeLeft() !== "Merry Christmas!" &&
+    getTimeLeft() !== "Christmas are over!"
+      ? `$(watch) ${getTimeLeft()}`
+      : `$(sparkle) ${getTimeLeft()}`;
+  statusBar.tooltip =
+    getTimeLeft() !== "Merry Christmas!" &&
+    getTimeLeft() !== "Christmas are over!"
+      ? `${getTimeLeft()} left until Christmas 🎄`
+      : null;
+  statusBar.show();
 }
 
 module.exports = { createStatusBar, updateStatusBar };
